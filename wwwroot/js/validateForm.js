f97dc9ca478a651d28da8d116464ae4b6eefdb81
davidlam172 <58743652+davidlam172@users.noreply.github.com>
2023-10-10 12:26:02 -0700

    Added required fields in forms

---
+++ wwwroot/js/validateForm.js
@@ -0,0 +1,27 @@
+﻿function validateForm() {
+    var fields = document.querySelectorAll("input[required]");
+    var errorMessages = [];
+
+    fields.forEach(function (field) {
+        if (!field.value.trim()) {
+            field.classList.add("error");
+            errorMessages.push(field.labels[0].textContent + " is required.");
+        } else {
+            field.classList.remove("error");
+        }
+    });
+
+    var errorDiv = document.getElementById("error-messages");
+    errorDiv.innerHTML = "";
+
+    if (errorMessages.length > 0) {
+        errorMessages.forEach(function (message) {
+            var p = document.createElement("p");
+            p.textContent = message;
+            errorDiv.appendChild(p);
+        });
+        return false; // Prevent form submission
+    }
+
+    return true; // Allow form submission
+}
